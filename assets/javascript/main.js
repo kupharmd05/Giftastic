0ddfe6ea8d23297f10f8dc42c5dff09493bc4a3e
kupharmd05 <kupharmd05@gmail.com>
2018-12-27 20:44:24 -0600

    Initial file load - Gif on click not working

---
+++ assets/javascript/main.js
@@ -0,0 +1,77 @@
+$(document).ready( function() {
+
+    var actors = ["Denzel Washington", "Brad Pitt", "Morgan Freeman", "Jennifer Lawrence", "Reese Witherspoon"];
+
+    function displayActors() {
+        
+        var actor = $(this).attr("data-name");
+        // var actor = "brad pitt";
+        var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + 
+        actor + "&api_key=tvPQjKn3MnAfxOxO9lle4f3V9XjJSm8q&limit=10";
+
+        $.ajax({
+            url: queryUrl,
+            method: "get"
+        }).then(function(response){
+            var actorInfo = $("<div>").text(JSON.stringify(response));
+            console.log(response);
+            var results = response.data;
+            for(let result of results) {
+                console.log(result);
+                var actorGifs = $("<img>")
+                    .attr({"src":result.images.downsized_still.url, "data-still":result.images.downsized_still.url, "data-animate":result.images.downsized.url, "data-state":"still"});
+                actorGifs.addClass("gif")
+                $("#actor-gifs").append(actorGifs);
+            }
+        });
+
+    }
+   
+    
+    function addActor(){
+        event.preventDefault();
+        var actor = $("#actor-input").val();
+
+        actors.push(actor);
+
+        renderButtons();
+    }
+
+    function renderButtons() {
+        $("#actor-view").empty();
+
+        for (var i=0; i < actors.length; i++) {
+            var a = $("<button>");
+            a.attr("data-name", actors[i]);
+            a.addClass("actor");
+            a.text(actors[i]);
+            $("#actor-view").append(a);
+        }
+    }
+
+    $(".gif").on("click", function(){
+
+        console.log("this works");
+        //     var state = $(this).attr("data-state");
+        //     if (state ==="still") {
+        //         $(this).attr("src", "data-animate");
+        //         $(this).attr("data-state", "animated")
+        //     } else if (state ==="animated") {
+        //         $(this).attr("src", "data-still");
+        //         $(this).attr("data-state", "still");
+        //     }
+        });
+    
+    
+
+    $("#add-actor").on("click", addActor);
+
+    renderButtons();
+
+    $(document).on("click", ".actor", displayActors);
+
+
+});
+
+
+
