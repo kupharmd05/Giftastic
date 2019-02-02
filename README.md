4ad4586b9b1410a99f181c9e659bdc3eb706ca01
kupharmd05 <kupharmd05@gmail.com>
2019-02-01 19:18:11 -0600

    cleaned up var vs. let in code

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 Actors!
 
 Click on you favorite actor name or search for anyone you want to see some giphy images.
+
+The assignment:
+Use the Giphy API to populate a page using the AJAX function
+
--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,10 +1,10 @@
 $(document).ready( function() {
 
-    var actors = ["Denzel Washington", "Brad Pitt", "Morgan Freeman", "Jennifer Lawrence", "Reese Witherspoon", "Chris Hemsworth", "Samuel Jackson"];
+    let actors = ["Denzel Washington", "Brad Pitt", "Morgan Freeman", "Jennifer Lawrence", "Reese Witherspoon", "Chris Hemsworth", "Samuel Jackson"];
 
     function displayActors() {
         
-        var actor = $(this).attr("data-name");
+        let actor = $(this).attr("data-name");
         
         var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + 
         actor + "&api_key=tvPQjKn3MnAfxOxO9lle4f3V9XjJSm8q&limit=10";
@@ -13,13 +13,12 @@ $(document).ready( function() {
             url: queryUrl,
             method: "get"
         }).then(function(response){
-            var actorInfo = $("<div>").text(JSON.stringify(response));
             console.log(response);
-            var results = response.data;
+            let results = response.data;
             for(let result of results) {
                 console.log(result);
-                var info = $("<div>");
-                var actorGifs = $("<img>")
+                let info = $("<div>");
+                let actorGifs = $("<img>")
                     .attr({"src":result.images.downsized_still.url, "data-still":result.images.downsized_still.url, "data-animate":result.images.downsized.url, "data-state":"still"});
                 actorGifs.addClass("gif")
                 actorGifs.addClass("rating")
@@ -27,14 +26,14 @@ $(document).ready( function() {
                 
 
 
-                var actorRating = $("<p>").text("Rating: " + result.rating);
+                let actorRating = $("<p>").text("Rating: " + result.rating);
                 actorRating.addClass("rating");
 
                 info.append(actorGifs, actorRating);
                 
                 $("#actor-gifs").prepend(info);
 
-                // $(".gif").on("click", moveImage);
+            
             }
         });
 
@@ -43,7 +42,7 @@ $(document).ready( function() {
     
     function addActor(){
         event.preventDefault();
-        var actor = $("#actor-input").val();
+        let actor = $("#actor-input").val();
 
         actors.push(actor);
 
@@ -53,9 +52,8 @@ $(document).ready( function() {
     function renderButtons() {
         $("#actor-buttons").empty();
 
-        // for (var i=0; i < actors.length; i++) {
             for (let actor of actors) {
-            var a = $("<button>");
+            let a = $("<button>");
             a.attr("data-name", actor);
             a.addClass("actor btn");
             a.text(actor);
@@ -65,7 +63,7 @@ $(document).ready( function() {
 
     function moveImage() {
             
-            var state = $(this).attr("data-state");
+            let state = $(this).attr("data-state");
             if (state ==="still") {
                 $(this).attr("src", $(this).attr("data-animate"));
                 $(this).attr("data-state", "animated")
@@ -73,7 +71,7 @@ $(document).ready( function() {
                 $(this).attr("src", $(this).attr("data-still"));
                 $(this).attr("data-state", "still");
             }
-            console.log("this works");
+            
         };
     
     $("#add-actor").on("click", addActor);
